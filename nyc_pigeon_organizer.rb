116ef9d3770009ba98af8540951c38a643fe32ff
Patrick A <akolo@hotmail.com>
2020-03-25 22:57:57 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
+require 'pry'
+require 'pp'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_listgeon_list = {}
+  
+  data.each do |color_gender_lives, value|
+   value.each do |details, all_names|
+     all_names.each do |name|
+       if pigeon_list[name] == nil 
+         pigeon_list[name] = {}
+       end 
+         if pigeon_list[name][color_gender_lives] == nil 
+           pigeon_list[name][color_gender_lives] = []
+          end 
+          pigeon_list[name][color_gender_lives].push(stats.to_s)
+      
+     end 
+    end 
+  end 
+  pegion_list
 end
