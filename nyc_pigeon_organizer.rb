c545ceeb537ba7f52851de5e10ef2eb7d52485c8
Patrick A <akolo@hotmail.com>
2020-03-25 23:10:39 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
+require 'pry'
+require 'pp'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  
+  data.each do |color_gender_lives, value|
+   value.each do |details, all_names|
+     all_names.each do |name|
+       
+        pigeon_list[name] ||= {}
+        pigeon_list[name][color_gender_lives] ||= []
+        pigeon_list[name][color_gender_lives].push(details.to_s)
+      end 
+    end 
+  end 
+  pigeon_list
 end
